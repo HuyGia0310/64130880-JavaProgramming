81c1709bed28848eaebcd7510e60b8f021cc69ba
HuyGia0310 <huyphamgia03102004@gmail.com>
2026-01-23 20:20:57 +0700

    ví dụ Hello

---
+++ bai1_Hello/src/bai1_Hello/SayHelloApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package bai1_Hello;
+
+/**
+ * 
+ */
+public class SayHelloApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin Chao.Toi la sinh vien NTU:Hoc Lap Trinh Java");
+
+	}
+
+}
