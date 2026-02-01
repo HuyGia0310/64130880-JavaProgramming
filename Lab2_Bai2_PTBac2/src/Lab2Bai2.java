d8ef6cf7bbbb4012ed29c07169d06da4db02a51d
HuyGia0310 <huyphamgia03102004@gmail.com>
2026-02-01 17:28:28 +0700

    lab2 bài 1 và 2

---
+++ Lab2_Bai2_PTBac2/src/Lab2Bai2.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Lab2Bai2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhập a: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Nhập b: ");
+        double b = sc.nextDouble();
+
+        System.out.print("Nhập c: ");
+        double c = sc.nextDouble();
+
+        if (a == 0) {
+            if (b == 0) {
+                if (c == 0)
+                    System.out.println("Vô số nghiệm");
+                else
+                    System.out.println("Vô nghiệm");
+            } else {
+                System.out.println("Nghiệm x = " + (-c / b));
+            }
+        } else {
+            double delta = b * b - 4 * a * c;
+
+            if (delta < 0) {
+                System.out.println("Phương trình vô nghiệm");
+            } else if (delta == 0) {
+                double x = -b / (2 * a);
+                System.out.println("Nghiệm kép x = " + x);
+            } else {
+                double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+                System.out.println("x1 = " + x1);
+                System.out.println("x2 = " + x2);
+            }
+        }
+    }
+}
